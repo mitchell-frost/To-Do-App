f502950dac98ce2c479dc84589c8aeb484472a29
Bhawna Patnaik <44670836+mitchell-frost@users.noreply.github.com>
2020-05-16 22:03:05 +0530

    making the checkboxes work

--- src/App.js
+++ src/App.js
@@ -18,8 +18,30 @@ import todosData from './todosData'
 } */
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todosData
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if(todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+      return {
+        todos: updatedTodos
+      }
+    })
+  }
+  
   render() {
-    const todoItems = todosData.map(item => <TodoItem key = {item.id} item = {item} />)
+    const todoItems = todosData.map(item => <TodoItem key = {item.id} item = {item} handleChange = {this.handleChange}/>)
     return (
     <div className="App">      
       <Header />
