061778acee32c72581407875e5a64e40a2f1d4b1
Bhawna Patnaik <44670836+mitchell-frost@users.noreply.github.com>
2020-05-16 19:59:23 +0530

    add event listener in TodoItem.js

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -6,7 +6,11 @@ import './App.css';
 function TodoItem(props) {
     return (
         <div className="todo-item">
-            <input type="checkbox" checked={props.item.completed}/>
+            <input 
+                type="checkbox" 
+                checked={props.item.completed} 
+                onChange={() => console.log("Changed!")}
+            />
             <p>{props.item.text}</p>
         </div>
     )
